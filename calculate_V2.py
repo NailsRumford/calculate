816a1d26b57bce050e9ea2b30f3b3c8bf2f122a0
Артем Волков <NailsRumford@internet.ru>
2023-02-06 11:47:53 +0300

    add docstring

--- calculate_V2.py
+++ calculate_V2.py
@@ -2,6 +2,22 @@ from tools import сhecking_expression
 
 
 def calculate(expression):
+    """
+    Эта функция вычисляет выражение, переданное в качестве аргумента. Выражение должно быть строкой, состоящей из цифр и следующих знаков: "+", "-", "*", "/".
+    
+    Аргументы:
+    expression (str): Выражение для вычисления.
+    
+    Возвращает:
+    result (int/float): Результат вычисления. Если выражение недопустимо, возвращает None.
+    
+    Пример:
+    >>> calculate("2+2")
+    4
+    >>> calculate("2/0")
+    Ошибка при вычислении выражения: division by zero
+    None
+    """
     сhecking_expression(expression)
     try:
         result = eval(expression)
@@ -9,3 +25,5 @@ def calculate(expression):
     except Exception as error:
         print("Ошибка при вычислении выражения:", error)
         return None
+
+
